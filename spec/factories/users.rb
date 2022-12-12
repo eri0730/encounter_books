f3f5db0eb4f4404a3c6d558b387b9437af234411
eri0730 <eri.0730_@docomo.ne.jp>
2022-12-12 23:15:43 +0900

    Userモデル単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
-  end
+    nickname {Faker::Name.initials(number: 2)}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}  end
 end
