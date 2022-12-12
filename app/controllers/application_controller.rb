ee7d79dc9d2d52a96198427c44b74511f031eb3f
eri0730 <eri.0730_@docomo.ne.jp>
2022-12-12 21:28:59 +0900

    サインアップ機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  #before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -10,4 +11,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+  
 end
