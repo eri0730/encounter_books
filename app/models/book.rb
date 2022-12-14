71be415c4348dd5032fc1dce8859bfd8fc902702
eri0730 <eri.0730_@docomo.ne.jp>
2022-12-15 01:20:35 +0900

    バリデーションを設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,11 @@ class Book < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :summary, presence: true
+  validates :recommend, presence: true, length: { maximum: 80 }
+  validates :image, presence: true
+
 end
