e5de9692d68f3c57727449ab4769cf47edb5841b
eri0730 <eri.0730_@docomo.ne.jp>
2022-12-30 03:36:32 +0900

    destroyアクションの記述

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,9 +27,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    # book = Book.find(params[:id])
-    # book.destroy
-    # redirect_to root_path
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to root_path
   end  
 
   def update
