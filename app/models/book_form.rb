f169892244e047daef3854447fed05b2433ea09f
eri0730 <eri.0730_@docomo.ne.jp>
2023-01-02 21:15:38 +0900

    BookFormクラスのオブジェクトがuser_idを扱えるように追加

--- app/models/book_form.rb
+++ app/models/book_form.rb
@@ -2,7 +2,7 @@ class BookForm
   include ActiveModel::Model
 
   #BookFormクラスのオブジェクトがBookモデルの属性を扱えるようにする
-  attr_accessor :title, :author, :summary, :recommend, :image
+  attr_accessor :title, :author, :summary, :recommend, :image, :user_id
 
   with_options presence: true do
     validates :image
@@ -13,7 +13,7 @@ class BookForm
   end
 
   def save
-    Book.create(title: title, author: author, summary: summary, recommend: recommend, image: image)
+    Book.create(title: title, author: author, summary: summary, recommend: recommend, image: image, user_id: user_id)
   end
 
 end
