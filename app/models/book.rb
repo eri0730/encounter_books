816bdbadf0fb75349e5ee8cd81a1195cd92015e7
eri0730 <eri.0730_@docomo.ne.jp>
2023-01-02 22:27:37 +0900

    バリデーションを記述

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,6 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_tag_relations
   has_many :tags, through: :book_tag_relations
-  
-  has_one_attached :image
 
+  has_one_attached :image
 end
