2670c147d377007bac5a5fe622e3cd378dfc675b
eri0730 <eri.0730_@docomo.ne.jp>
2023-01-05 22:36:31 +0900

    アソシエーションを設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_tag_relations, dependent: :destroy
   has_many :tags, through: :book_tag_relations, dependent: :destroy
+  has_many :favorites, dependent: :destroy  
 
   has_one_attached :image
 end
