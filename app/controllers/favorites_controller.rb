2cc46d74fe4f4a08fe41097ec5d56a2cc09351f0
eri0730 <eri.0730_@docomo.ne.jp>
2023-01-07 00:29:35 +0900

    createアクション、destroyアクションの記述

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,28 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :book_params, only: [:create, :destroy]
+
+  def create
+    @user = @book.user
+    current_user.favorite(@book)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
+ end
+
+  def destroy
+    current_user.favorites.find_by(book_id: @book.id).destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
+  end
+
+  private
+
+  def book_params
+    @book = Book.find(params[:book_id])
+  end
+
 end
