54281a835b87df049a02bb34782d2d229274d5b9
eri0730 <eri.0730_@docomo.ne.jp>
2023-01-07 00:28:02 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "books#index"
   resources :books
+  # resources :books do
+    # resources :favorites, only: [:create, :destroy]
+  # end  
   resources :users, only: :show
+  post   "favorites/:book_id/create"  => "favorites#create"
+  delete "favorites/:book_id/destroy" => "favorites#destroy"
+
+
 end
