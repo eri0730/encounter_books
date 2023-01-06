fb47ff2210617040daa1f00253cc0a67d3b28067
eri0730 <eri.0730_@docomo.ne.jp>
2023-01-07 00:29:58 +0900

    お気に入り登録のメソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,20 @@ class User < ApplicationRecord
   has_many :favorites
 
   validates :nickname, presence: true
+
+
+  def favorite(book)
+    Favorite.create!(user_id: id, book_id: book.id)
+  end
+
+  # 投稿のお気に入り解除する
+  def unfavorite(book)
+    Favorite.find_by(user_id: id, book_id: book.id).destroy
+  end
+
+  # 現在のユーザーがお気に入り登録してたらtrueを返す
+  def favorite?(book)
+    !Favorite.find_by(user_id: id, book_id: book.id).nil?
+  end
+
 end
