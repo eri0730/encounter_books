d8befb0fdbf08f92e0b2169c13ab082487510045
eri0730 <eri.0730_@docomo.ne.jp>
2023-01-10 00:42:23 +0900

    mypageアクションを記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
+
+  def mypage
+    user = User.find(params[:id])
+    return redirect_to root_path unless current_user.id == user.id
+    favorites = Favorite.where(user_id: current_user.id).pluck(:book_id)
+    @books = Book.find(favorites)
+  end  
+
 end
